577c28df21fd6c376bd05a085bd47e00ca8db1ee
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-23 18:14:04 +0300

    _07005_Guess_the_Password  Added

---
+++ 07005_Guess the Password/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07005_Guess_the_Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
