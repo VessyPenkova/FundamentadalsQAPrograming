a243c837a0ba606521ed7ca68c8f4af8e2bbfd75
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-23 18:16:42 +0300

    _07006_Boiling_Water  Added

---
+++ 07006_Boiling Water/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace _07006_Boiling_Water
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int tempreture = int.Parse(Console.ReadLine());
+            if (tempreture > 100)
+            {
+                Console.WriteLine("The water is boiling");
+            }
+            else
+            {
+                Console.WriteLine("The water is not hot enough");
+            }
+        }
+    }
+}
+
+//Write a program to check for boiling water, which: 
+//Reads a number: the water temperature (in °C)
+//Prints "The water is boiling" if the number > 100
+//Prints "The water is not hot enough" in all other cases 
