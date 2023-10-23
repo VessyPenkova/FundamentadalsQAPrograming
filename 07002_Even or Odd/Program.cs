ec5c83f255a6e6896610831d6150ee07b88acf5a
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-23 18:02:21 +0300

    _07003_Number_1_9_as_Words Added

---
+++ 07002_Even or Odd/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07002_Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 07003_Number 1…9 as Words/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07003_Number_1_9_as_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
