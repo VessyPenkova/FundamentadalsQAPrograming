1591b3859a7aaaaa3f974ed6e1a9c89c233c4212
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-23 18:20:54 +0300

    _07007_Speed_Info  Added

---
+++ 07007_Speed Info/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07007_Speed_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
