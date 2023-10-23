2e010a21965170d05caeffa41bb7066872c3457b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-23 18:36:12 +0300

    _07009_Area_of_Figures Added

---
+++ 07009_Area of Figures/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07009_Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
