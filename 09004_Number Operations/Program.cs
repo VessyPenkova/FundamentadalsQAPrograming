a3c49b172356742421be180f46a098600e446fbf
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 15:34:10 +0300

    _09004_Number_Operations Added

---
+++ 09004_Number Operations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09004_Number_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
