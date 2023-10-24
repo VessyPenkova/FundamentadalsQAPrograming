8f020a84f90372d548d374422e50d49a1a6449bb
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 17:57:34 +0300

    _10005_Numbers_From_1_to_N_over_3 Added

---
+++ 10005_Numbers From 1 to N over 3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10005_Numbers_From_1_to_N_over_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
