ca16b1bfcec97f734d49527cfbace7c92fe2cddc
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 14:30:04 +0300

    _08005_Fruit_or_Vegetable Added

---
+++ 08005_Fruit or Vegetable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08005_Fruit_or_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
