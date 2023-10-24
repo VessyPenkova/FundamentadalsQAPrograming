8c9e08adf36a153dbc0e746a8281bae0bbcd0f50
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 15:26:30 +0300

    _09003_Biggest_of_Five_Numbers Added

---
+++ 09003_Biggest of Five Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09003_Biggest_of_Five_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
