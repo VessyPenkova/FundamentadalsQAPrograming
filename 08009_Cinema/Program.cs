6425237c297736c4a0dd8b33c9094cee17c7e4a7
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 15:08:21 +0300

    _08009_Cinema Added

---
+++ 08009_Cinema/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08009_Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
