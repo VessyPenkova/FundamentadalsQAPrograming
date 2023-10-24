02c5b7e676257502dc01651c34c64525aad3b3dc
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 17:49:37 +0300

    _10003_Sum_Numbers Added

---
+++ 10003_Sum Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10003_Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
