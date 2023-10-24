cfe67b13045e6ae35730350d4d2b1db978111847
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 14:34:21 +0300

    _08006_Day_Of_Week Added

---
+++ 08006_Day Of Week/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08006_Day_Of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
