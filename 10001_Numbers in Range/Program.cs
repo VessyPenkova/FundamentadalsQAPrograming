1f842584d8b21c7421091181c3621f1daff7d136
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 17:44:20 +0300

    _10002_First_N_Numbers_Sum Added

--- 10001_Numbers in Range/Program.cs
+++ 10001_Numbers in Range/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace _10001_Numbers_in_Range
+﻿using System.ComponentModel;
+
+namespace _10001_Numbers_in_Range
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int beginingOfLoop = int.Parse(Console.ReadLine());
+           int endOfLoop = int.Parse(Console.ReadLine());
+            for (int i = beginingOfLoop; i <= endOfLoop; i++)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
+
+//Write a program that prints numbers in a given range:
+//•	Reads two integer numbers from the console
+//•	First integer number represents the start of the range
+//•	Second integer number represents the end of the range
+//•	Print the numbers in the given range (include start and end number), each on the new line
+
+//Note: The first given integer will always be bigger than the second given integer.
---
+++ 10002_First N Numbers Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace _10002_First_N_Numbers_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n)
+                {
+                    sb.Append($"{i}=");
+                }
+                else
+                {
+                  sb.Append($"{i}+");
+                } 
+                sum += i;
+            }
+            sb.Append(sum);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
+
+//Write a program, which sums the numbers 1…n:
+//•	Reads an integer number n from the console
+//•	Sums all numbers from 1 to n
+//•	Prints the numbers and the sum on the console as shown below
