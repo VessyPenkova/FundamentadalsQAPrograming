5f1a29f3b87bbcb9d9487d4e003da9879ed695b5
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 14:55:02 +0300

    _08008_Sorted_Numbers Added

---
+++ 08008_Sorted Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08008_Sorted_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
