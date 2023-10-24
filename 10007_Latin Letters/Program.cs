1d0102cbd67ef9e82285ec7c6567f5c3b558b955
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 18:07:34 +0300

    _10007_Latin_Letters Added

---
+++ 10007_Latin Letters/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10007_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
