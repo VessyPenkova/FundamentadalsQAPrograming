ae05e171267d762dc469405bc0b7f4d1643117bd
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 13:58:08 +0300

    _08001_Marketplace Added

---
+++ 08001_Marketplace/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+
+namespace _08001_Marketplace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string dayOfTheWeek = Console.ReadLine();
+            double price = 0;
+            if (product == "Banana")
+            {
+                if (dayOfTheWeek == "Weekday")
+                {
+                    price = 2.50;
+                }
+                else if (dayOfTheWeek == "Weekend")
+                {
+                    price = 2.70;
+                }
+            }
+            else if (product == "Apple")
+            {
+                if (dayOfTheWeek == "Weekday")
+                {
+                    price = 1.30;
+                }
+                else if (dayOfTheWeek == "Weekend")
+                {
+                    price = 1.60;
+                }
+            }
+            else if (product == "Kiwi")
+            {
+                if (dayOfTheWeek == "Weekday")
+                {
+                    price = 2.20;
+                }
+                else if (dayOfTheWeek == "Weekend")
+                {
+                    price = 3.00;
+                }
+            }
+             
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
+
+//Write a program that:
+//•	Reads two strings from the console: product(string) and day(string).
+//•	Print the price, formatted to the second digit, based on the price table:
+//||================================================
+//||   Product     ||   Weekday    ||   Weekend   ||
+//==================================================
+//||  Banana       ||     2.50     ||      2.70   ||
+//||================================================
+//||  Apple        ||     1.30     ||      1.60   ||
+//||================================================
+//||  Kiwi         ||     2.20     ||      3.00   ||
+//||================================================
