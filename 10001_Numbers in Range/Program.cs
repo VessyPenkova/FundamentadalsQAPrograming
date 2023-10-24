179864cfe44c520f18c2fe7a9ceaa3262be3f69d
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 17:33:39 +0300

    _10001_Numbers_in_Range Added

---
+++ 10001_Numbers in Range/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10001_Numbers_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
