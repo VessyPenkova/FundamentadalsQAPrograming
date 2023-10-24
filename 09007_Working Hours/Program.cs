542cd259184b90e10b8fb52934475bf14051ed24
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 16:34:20 +0300

    _09007_Working_Hours Added

---
+++ 09007_Working Hours/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09007_Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
