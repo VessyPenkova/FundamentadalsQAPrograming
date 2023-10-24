1ba5a3343783e4029f50d65fd99658ab406f2f9f
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 18:02:33 +0300

    _10006_Exam_Countdown Added

---
+++ 10006_Exam Countdown/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10006_Exam_Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
