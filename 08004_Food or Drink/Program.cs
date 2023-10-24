c32933997e5c66339b3e899cd37c000a7c233be0
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 14:26:11 +0300

    _08004_Food_or_Drink Added

---
+++ 08004_Food or Drink/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08004_Food_or_Drink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
