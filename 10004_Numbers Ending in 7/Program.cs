24bd191512eeb961eed2d2ebe8a1a48e67011182
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 17:52:49 +0300

    _10004_Numbers_Ending_in_7 Added

---
+++ 10004_Numbers Ending in 7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10004_Numbers_Ending_in_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
