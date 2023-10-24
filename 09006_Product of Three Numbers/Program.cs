2f00566f431ebaae3963c2cb4f25cf22369908ff
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 16:19:42 +0300

    _09006_Product_of_Three_Numbers Added

---
+++ 09006_Product of Three Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09006_Product_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
