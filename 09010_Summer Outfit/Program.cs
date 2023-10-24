c277667f4e9fa43cf77d458e29fd887882668350
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 17:13:28 +0300

    _09010_Summer_Outfit Added

---
+++ 09010_Summer Outfit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09010_Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
