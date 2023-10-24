d47a3270bfdf927ee18b0cc434c956845facb063
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-24 19:10:14 +0300

    _11005_Number_in_Range Added

---
+++ 11005_Number in Range/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11005_Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
