c46c7d531483df8d06c5c3cf470fd73f4eaeb558
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 12:23:31 +0300

    _13002_Even_Powers_of_2 Added

---
+++ 13002_Even Powers of 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13002_Even_Powers_of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
