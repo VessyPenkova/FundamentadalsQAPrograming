a185428c99859bba77d95f664004b05ec5165013
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 17:44:44 +0300

    _14001_Methods_Lab_Sign_of_Integer_Numbers Added

---
+++ 14001_Methods_Lab_Sign of Integer Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _14001_Methods_Lab_Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToCheck = int.Parse(Console.ReadLine());
+            GetTheSignOfInteger(numberToCheck);
+        }
+
+        private static void GetTheSignOfInteger(int numberToCheck)
+        {
+            if (numberToCheck < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else if (numberToCheck > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (numberToCheck == 0)
+            {
+                Console.WriteLine("zero");
+            }
+        }
+    }
+}
+
+//Write a program that:
+//•	Reads an integer number N from the console
+//•	Create a method that prints the sign of an entered number N:
+//•	Print:
+//o If the number is positive (N > 0): "The number {number} is positive."
+//o If the number is negative (N < 0): "The number {number} is negative."
+//o If the number is zero (N = 0): "The number {number} is zero."
