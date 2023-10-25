91765aaf69d87e6bab9d5fb95df40da42c4881d3
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 14:32:33 +0300

    _13008_Prime_Numbers Added

---
+++ 13008_Prime Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13008_Prime_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
