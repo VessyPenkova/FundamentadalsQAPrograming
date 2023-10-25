90f81ae2e50afd75468200ac2b3c3648eef94a2b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 13:54:48 +0300

    _13006_Travel_Savings Added

---
+++ 13006_Travel Savings/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13006_Travel_Savings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
