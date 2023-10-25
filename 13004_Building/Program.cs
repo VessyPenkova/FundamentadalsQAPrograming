101298206061cf69c4b0b56cfd293f689742e0c3
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 12:44:03 +0300

    _13004_Building Added

---
+++ 13004_Building/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13004_Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
