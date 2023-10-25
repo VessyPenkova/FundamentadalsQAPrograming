fc5cfda7acd8d77c99e2c2ea1c563a7e250fd4a9
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 11:01:56 +0300

    _12006_Special_Number Added

--- 11002_Odd Number/Program.cs
+++ 11002_Odd Number/Program.cs
@@ -9,7 +9,8 @@
             {
                 if (number% 2 != 0 )
                 {
-                    Console.WriteLine(number); break;
+                    Console.WriteLine(number); 
+                    break;
                 }
 
                 number = int.Parse(Console.ReadLine());
---
+++ 12006_Special Number/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _12006_Special_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();//as string(1, 3, 5, 6) Lenght of sequence is 3 strart form 0
+
+            //1 is in position 0
+            //3 is in position 1
+            //5 is in position 2
+            //6 is in position 3
+
+            int number = int.Parse(sequence);//as number to check if it is devideble 
+
+            bool isSpecialNumber = true;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                int RealNumber = Convert.ToInt32(sequence[i]) - '0';// take the char on [i] place of the sequence as number from Ascii table
+
+                if (number % RealNumber != 0)
+                {
+                    isSpecialNumber = false;
+                }
+                if (number % RealNumber == 0)
+                {
+                    isSpecialNumber = true;
+                }
+            }
+            if (isSpecialNumber == true)
+            {
+                Console.WriteLine($"{number} is special");
+            }
+            else if(isSpecialNumber == false)
+            {
+                Console.WriteLine($"{number} is not special");
+            }
+
+        }
+    }
+}
+
+//Write a program to check if given number is special: 
+//•	Special numbers are divisible by all of their digits without remainder
+//•	Reads an integer number and check whether it is a special number
+//•	If the number IS special print: "{num} is special"
+//•	If the number is NOT special print: "{num} is not special"
+//Note: There will not be numbers with digit 0 in them.
