aedcf097f598dbf164485610e9d3191e1f4cfd1b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 09:21:48 +0300

    _12003_Biggest_Number Added

---
+++ 12003_Biggest Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace _12003_Biggest_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+//Write a program to find the biggest among given n numbers:
+//•	Read an integer number n (the amount of input numbers) and n integer numbers from the console
+//•	Find and print the biggest number
