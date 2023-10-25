34e56c37fe356b1c34e6239a096f1aa7293baf9d
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 12:28:36 +0300

    _13003_Triangle_of_Stars

---
+++ 13003_Triangle of Stars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13003_Triangle_of_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
