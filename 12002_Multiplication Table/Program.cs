1471379c2023c9e0c6dbf16c819c11ffa26b3b6c
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 09:13:43 +0300

    _12002_Multiplication_Table Added

---
+++ 12002_Multiplication Table/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12002_Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
