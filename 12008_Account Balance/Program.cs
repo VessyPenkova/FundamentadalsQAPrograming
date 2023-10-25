8f89200437729841226a0989d15c8e76ceba5e6e
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-25 11:46:03 +0300

    _12008_Account_Balance Added

---
+++ 12008_Account Balance/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _12008_Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+//Write a program to calculate an account balance:
+//•	Read a sequence of incomes / expenses, until "End" is read
+//•	Add the money to the balance (starting form 0)
+//•	Print "Increase: {money}" or "Decrease: {money}", where value is formatted to the second decimal digit
+//•	Finally, print the account balance, formatted to the second decimal digit in the following format: "Balance: {account balance}"
