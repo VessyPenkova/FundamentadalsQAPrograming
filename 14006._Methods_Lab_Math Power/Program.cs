a326afe121c62d0f24b1a821cb6851c8d5b6fc0c
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-26 15:23:08 +0300

    _14007_Methods_Lab_Greater_of_Two_Values Added

--- 14006._Methods_Lab_Math Power/Program.cs
+++ 14006._Methods_Lab_Math Power/Program.cs
@@ -6,20 +6,48 @@
         {
             int baseNumbre = int.Parse(Console.ReadLine());
             int onPower = int.Parse(Console.ReadLine());
-            Console.WriteLine(RiseNumberOnPower(baseNumbre, onPower));
+
+            int result = RiseNumberOnPower(baseNumbre, onPower);
+            Console.WriteLine(result);
 
             static int RiseNumberOnPower(int n, int p)
             {
-                int result = 0;
-                for (int i = n; i < p-1; i++)
+                int result = n;
+                for (int i = 0; i < p-1; i++)
                 {
-                   result =  n * n;
+                   result *=  n ;
                 }
                 return result ;
             }
         }
     }
 }
+
+
+//namespace _14006._Methods_Lab_Math_Power second solution
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int baseNumbre = int.Parse(Console.ReadLine());
+//            int onPower = int.Parse(Console.ReadLine());
+
+//            Console.WriteLine(RiseNumberOnPower(baseNumbre, onPower));
+
+//            static int RiseNumberOnPower(int n, int p)
+//            {
+//                int result = n;
+//                for (int i = 0; i < p - 1; i++)
+//                {
+//                    result *= n;
+//                }
+//                return result;
+//            }
+//        }
+//    }
+//}
+
 
 //Write a program that:
 //•	Reads two integer numbers from the console: base number and power
---
+++ 14007_Methods_Lab_Greater of Two Values/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+string valueType = Console.ReadLine();
+
+if (valueType == "int")
+{
+    int num1 = int.Parse(Console.ReadLine());
+    int num2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(GreaterFromTwoNumbers(num1, num2));
+}
+else if (valueType == "char")
+{
+    char char1 = char.Parse(Console.ReadLine());
+    char char2 = char.Parse(Console.ReadLine());
+
+    Console.WriteLine(GreaterFromTwoChars(char1, char2));
+}
+else if (valueType == "string")
+{
+    string str1 = Console.ReadLine();
+    string str2 = Console.ReadLine();
+
+    Console.WriteLine(GreaterFromTwoStrings(str1, str1));   
+}
+
+static string GreaterFromTwoStrings(string str1, string str2)
+{
+   
+    if(str1.CompareTo(str2) < 0)
+    {
+       return  str1;
+    }
+    else
+    {
+        return str2;
+    }
+   
+}
+static char  GreaterFromTwoChars(char char1, char char2)
+{
+    char result = '0';
+    if (char1 > char2)
+    {
+        result = char1;
+    }
+    else if (char1 < char2)
+    {
+        result = char2;
+    }
+    return result;
+}
+static int GreaterFromTwoNumbers(int num1, int num2)
+{
+    int result = 0;
+    if (num1 > num2)
+    {
+        result = num1;
+    }
+    else if (num1 < num2)
+    {
+        result = num2;
+    }
+    return result;
+}
+
+//•	Reads a type (string) and two values of this type from the console
+//•	Entered type can be one of the following values: "int", "char" or "string"
+//•	Create methods which can compare int, char or string
+//•	Return the biggest of the two values
