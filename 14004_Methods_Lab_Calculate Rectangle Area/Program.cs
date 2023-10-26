424fd090f56e666915a77f1f1c4b84721c131352
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-26 13:49:34 +0300

    _14004_Methods_Lab_Calculate_Rectangle_Area Added

---
+++ 14004_Methods_Lab_Calculate Rectangle Area/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14004_Methods_Lab_Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
