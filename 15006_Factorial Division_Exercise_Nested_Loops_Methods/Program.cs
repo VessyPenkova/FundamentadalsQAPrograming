dcda96b18c29364edd0e8dd76e4f6d1ee58ff11b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-26 21:46:08 +0300

    15007_Factorial_Division_Exercise_Nested_Loops_Methods Added

---
+++ 15006_Factorial Division_Exercise_Nested_Loops_Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _15006_Factorial_Division_Exercise_Nested_Loops_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
