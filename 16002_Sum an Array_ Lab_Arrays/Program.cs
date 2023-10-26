a246854d65b06ed1e0d48ab20965ad4b28cc91bd
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-26 23:50:07 +0300

    16002_Sum_an_Array__Lab_Arrays Added

---
+++ 16002_Sum an Array_ Lab_Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _16002_Sum_an_Array__Lab_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
