83c9af37d4bcd4ce3ee54c283af968fcb2192ee9
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-26 14:04:41 +0300

    _14005_Methods_Lab_Repeat_String Added

---
+++ 14005_Methods_Lab_Repeat String/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14005_Methods_Lab_Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
