484c9ca5d349d73268753f6b3a9a6b690c1d9a68
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-26 15:51:08 +0300

    _14008_Methods_Lab_Multiply_Evens_by_Odds Added

---
+++ 14008_Methods_Lab_Multiply_Evens_by_Odds/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14008_Methods_Lab_Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
