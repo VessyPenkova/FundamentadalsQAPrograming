0743d3fded6e9341a47565f5ea32330474f02d19
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 11:43:06 +0300

    17004A_List_Manipulation_Basics__Lab_Lists Added

---
+++ 17004A_List_Manipulation_Basics__Lab_Lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17004A_List_Manipulation_Basics__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
