a11618971fda2e42b8fd7d198149960e4b17a434
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 11:21:29 +0300

    17003_Remove_Negatives_and_Reverse__Lab_Lists Added

--- 17002A_List_of_Products__Lab_Lists/Program.cs
+++ 17002A_List_of_Products__Lab_Lists/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine()); //брой на продуктите
+
+            List<string> products = new List<string>(); //продукти
+
+            for (int i = 1; i <= n; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            //пълен списък с продуктите
+            //{"Potatoes", "Tomatoes", "Onions", "Apples"}
+            //сортиране по азбучен ред (ascending order -> A - Z)
+            products.Sort();
+            //{"Apples", "Onions", "Potatoes", "Tomatoes"}
+
+            int number = 1; //поредния номер на продукта
+            foreach (string product in products)
+            {
+                Console.WriteLine(number + "." + product);
+                number++;
+            }
         }
     }
 }
+
+//Write a program that:
+//•	Read an integer number n and n lines of products
+//•	Print a numbered list of all the products ordered by name
---
+++ 17003_Remove Negatives and Reverse_ Lab_Lists/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _17003_Remove_Negatives_and_Reverse__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integersInput = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToList();
+            
+            for (int i = 0; i < integersInput.Count; i++)
+            {
+                if (integersInput[i] < 0)
+                {
+                    integersInput.RemoveAt(i--);
+                }
+            }
+
+            integersInput.Reverse();
+
+            if (integersInput.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", integersInput));
+            }
+
+        }
+    }
+}
+
+//Write a program that:
+//•	Read a list of integers 
+//•	Remove all negative numbers from it 
+//•	Print the remaining elements in reversed order
+//•	If there are no elements left in the list, print "empty"
