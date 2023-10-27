628e3ee5ce528f4489dfae32b95b06c5dbad15cb
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 11:10:07 +0300

    17002A_List_of_Products__Lab_Lists Added

---
+++ 17002A_List_of_Products__Lab_Lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17002A_List_of_Products__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
