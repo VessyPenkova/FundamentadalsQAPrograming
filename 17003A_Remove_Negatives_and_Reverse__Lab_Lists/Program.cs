25d0a5ee1107797e89e0e0507a11f43975653ac2
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 11:24:24 +0300

    17003A_Remove_Negatives_and_Reverse__Lab_Lists Added

---
+++ 17003A_Remove_Negatives_and_Reverse__Lab_Lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17003A_Remove_Negatives_and_Reverse__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
