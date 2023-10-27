8414b334ed1075897ac465d4f25e1790191786d8
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 11:06:32 +0300

    17002_List_of_Products__Lab_Lists Added

---
+++ 17002_List of Products_ Lab_Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace _17002_List_of_Products__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> listOfProducts = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                listOfProducts.Add(product);
+            }
+            listOfProducts.Sort();
+            for (int i = 0; i < listOfProducts.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{listOfProducts[i]}");
+            }
+        }
+    }
+}
+
+//Write a program that:
+//•	Read an integer number n and n lines of products
+//•	Print a numbered list of all the products ordered by name
