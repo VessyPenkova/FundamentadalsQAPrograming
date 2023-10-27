4a515543370e698e86e7a66872e50a4a9a8f23ef
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 13:12:50 +0300

    17005A_List_Manipulation_Advanced__Lab_Lists Added

---
+++ 17005A_List_Manipulation_Advanced__Lab_Lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17005A_List_Manipulation_Advanced__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
