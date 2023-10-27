2c1b969f5f47238c6e29289dedd36aa24e2b2544
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 11:41:12 +0300

    17004_List_Manipulation_Basics__Lab_Lists Added

--- 17003A_Remove_Negatives_and_Reverse__Lab_Lists/Program.cs
+++ 17003A_Remove_Negatives_and_Reverse__Lab_Lists/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace _17003A_Remove_Negatives_and_Reverse__Lab_Lists
+﻿using System.ComponentModel;
+
+namespace _17003A_Remove_Negatives_and_Reverse__Lab_Lists
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            //{10, -5, 7, 9, -33, 50}
+
+            numbers.RemoveAll(number => number < 0);
+            //{10, 7, 9, 50}
+
+            numbers.Reverse();
+            //{50, 9, 7, 10}
+
+            //нямаме елементи
+            if (numbers.Count == 0)
+            {
+                //празен списък
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                //списък пълен с числа
+                Console.WriteLine(string.Join(" ", numbers));
+            }
         }
     }
 }
+
+//Write a program that:
+//•	Read a list of integers 
+//•	Remove all negative numbers from it 
+//•	Print the remaining elements in reversed order
+//•	If there are no elements left in the list, print "empty"
---
+++ 17004_List Manipulation Basics_ Lab_Lists/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _17004_List_Manipulation_Basics__Lab_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                List<string> commandsParts = command.Split(" ").ToList();
+                string firstPartOfTheCommand = commandsParts[0];
+                //string secondPartOfTheCommand = command.Substring(1);
+                if (firstPartOfTheCommand == "Add")
+                {
+                    int numberToAdd = int.Parse(commandsParts[1]);
+                    numbersInput.Add(numberToAdd);
+                }
+                else if (firstPartOfTheCommand == "Remove")
+                {
+                    int numberToRemove = int.Parse(commandsParts[1]);
+                    numbersInput.Remove(numberToRemove);
+                }
+                else if (firstPartOfTheCommand == "RemoveAt")
+                {
+                    int numberToRemoveAtIndex = int.Parse(commandsParts[1]);
+                    numbersInput.RemoveAt(numberToRemoveAtIndex);
+                }
+                else if (firstPartOfTheCommand == "Insert")
+                {
+                    int numberToInsert = int.Parse(commandsParts[1]);
+                    int numberToInserAtIndex = int.Parse(commandsParts[2]);
+                    numbersInput.Insert(numberToInserAtIndex, numberToInsert);
+                }
+                command = Console.ReadLine();
+            }
+            Console.Write(String.Join(" ", numbersInput));
+           //Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
+
+//Write a program that: 
+//•	Reads a list of integers
+//•	Then until you receive "end", you will receive different commands:
+//      V       "Add {number}": add a number to the end of the list
+//      V       "Remove {number}": remove a number from the list
+//      V       "RemoveAt {index}": remove a number at a given index
+//      'V      "Insert {number} {index}": insert a number at a given index
+//•	When you receive the "end" command, print the final state of the list (separated by spaces)
+//Note: All the indices will be valid!
