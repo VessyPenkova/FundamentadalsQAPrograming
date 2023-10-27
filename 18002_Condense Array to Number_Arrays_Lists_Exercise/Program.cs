50bd82218ffd1d7b125a77388d7e6d1907a90e0e
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 14:36:15 +0300

    18002_Condense_Array_to_Number_Arrays_Lists_Exercise Added

---
+++ 18002_Condense Array to Number_Arrays_Lists_Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _18002_Condense_Array_to_Number_Arrays_Lists_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
