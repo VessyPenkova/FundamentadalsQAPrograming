a2667f239550887bbe8246e1e424e6b3fcc457c5
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-27 15:08:33 +0300

    18003_Merging_Lists_Arrays_Lists_Exercise Added

--- 18002_Condense Array to Number_Arrays_Lists_Exercise/Program.cs
+++ 18002_Condense Array to Number_Arrays_Lists_Exercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> input = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+            while (input.Count>1)
+            {
+                List<int> output = new List<int>();
+                for (int i = 0; i < input.Count-1; i++)
+                {
+                    int num1 = input[i];
+                    int num2 = input[i + 1];
+                    int sum = num1 + num2;
+
+                    output.Add(sum);
+                }
+                input = output;
+            }
+            Console.WriteLine(input[0]);
         }
     }
 }
+
+//Write a program that:
+//      V       Read a sequence of integer numbers from the console
+//•	Condense them by summing adjacent couples of elements until a single integer is obtained
+//(i0 +i1 ) + (i1+i2[inout.Count-1])
+//(2 + 10)  + (10 +3)
+//  (12)    +   (13)  = 25
+//Example: If we have 3 elements {2, 10, 3}. We sum the first two and the second two elements and obtain 
+//	           {2+10, 10+3} = { 12, 13}. Then we sum again all adjacent elements and obtain {12+13} = { 25}.
---
+++ 18003_Merging Lists_Arrays_Lists_Exercise/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _18003_Merging_Lists_Arrays_Lists_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> input1 = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> input2 = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+            List<int>reslt = new List<int>();
+            if (input1.Count> input2.Count)
+            {
+                for (int i = 0; i <= input2.Count-1; i++)
+                {
+                    reslt.Add(input1[i]);
+                    reslt.Add(input2[i]);   
+                }
+                for (int j = input2.Count ; j <= input1.Count - 1; j++)
+                {
+                    reslt.Add(input1[j]);
+                }
+            }
+            else if (input2.Count > input1.Count)
+            {
+                for (int i = 0; i <= input1.Count-1; i++)
+                {
+                    reslt.Add(input1[i]);
+                    reslt.Add(input2[i]);
+                   
+                }
+                for (int j = input1.Count ; j <=input2.Count - 1; j++)
+                {
+                    reslt.Add(input2[j]);
+                }
+            }
+            else if (input2.Count == input1.Count)
+            {
+                for (int i = 0; i <= input1.Count - 1; i++)
+                {
+                    reslt.Add(input1[i]);
+                    reslt.Add(input2[i]);
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", reslt));
+        }
+    }
+}
+
+//Write a program that:
+//•	Reads two sequences with integer numbers from the console
+//•	Create a result list that contains the numbers from both of the sequences
+//•	The first element should be from the first sequence, the second from the second sequence, and so on
+//•	If the length of the two sequences is not equal, just add the remaining elements at the end of the sequence
