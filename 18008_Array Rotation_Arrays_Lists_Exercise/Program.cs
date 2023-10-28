34b119d462fd889d8e59b05689addc0829079b6a
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-28 22:14:09 +0300

    UnitTestingMethods_Lab

--- 18008_Array Rotation_Arrays_Lists_Exercise/Program.cs
+++ 18008_Array Rotation_Arrays_Lists_Exercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<int> numbersForRotation = Console.ReadLine()
+          .Split(" ")
+          .Select(int.Parse)
+          .ToList();
+
+            int rotationCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rotationCount; i++)
+            {
+                int currentNumber = numbersForRotation[0];
+                numbersForRotation.RemoveAt(0);
+                numbersForRotation.Add(currentNumber);
+            }
+            Console.WriteLine(String.Join(" ", numbersForRotation));
         }
     }
 }
+
+//•	Reads sequence of integer numbers from the first line of the console
+//•	Reads an integer number (rotations you have to perform) from the second line of the console
+//•	Each rotation is when the first element goes at the end
+//•	Print the resulting sequence
---
+++ 18009S_Max Sequence of Equal Elements_Arrays_Lists_Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _18009S_Max_Sequence_of_Equal_Elements_Arrays_Lists_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+//•	Reads sequence of integer numbers from the first line of the console
+//•	Find the longest sequence of equal elements in a sequence of integers
+//• Note: If several longest sequences exist, print the leftmost one.
