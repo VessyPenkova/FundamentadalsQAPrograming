a09f3774fb9254d030ec261e875f52f01e81a63c
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-28 11:19:41 +0300

    18006S_Cards_Game_Arrays_Lists_Exercise Added

---
+++ 18006S_Cards Game_Arrays_Lists_Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _18006S_Cards_Game_Arrays_Lists_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
