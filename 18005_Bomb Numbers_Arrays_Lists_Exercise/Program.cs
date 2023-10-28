d3d58422e9336e12ba127df8f4fce0745fc3bd0f
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-28 10:56:19 +0300

    18005_Bomb_Numbers_Arrays_Lists_Exercise Added

---
+++ 18005_Bomb Numbers_Arrays_Lists_Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _18005_Bomb_Numbers_Arrays_Lists_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
