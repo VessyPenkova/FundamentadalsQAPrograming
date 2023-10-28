bbe4ff51e429f691f92ff31aa3441152fbb6183c
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-28 12:04:44 +0300

    18008_Array_Rotation_Arrays_Lists_Exercise Added

--- 18007_Zig-Zag Arrays_Arrays_Lists_Exercise/Program.cs
+++ 18007_Zig-Zag Arrays_Arrays_Lists_Exercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int counter = int.Parse(Console.ReadLine());
+
+            List<int> firstList = new List<int>();
+            List<int> secondList = new List<int>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                List<int> numbersTogether = Console.ReadLine()
+              .Split(" ")
+              .Select(int.Parse)
+              .ToList();
+
+              int number1 = numbersTogether[0];
+              int number2 = numbersTogether[1];
+                if ( i% 2 == 0)
+                {
+                    firstList.Add(number1);
+                    secondList.Add(number2);
+
+                }
+                else
+                {
+                    firstList.Add(number2);
+                    secondList.Add(number1);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", firstList));
+            Console.WriteLine(String.Join(" ", secondList));
         }
     }
 }
---
+++ 18008_Array Rotation_Arrays_Lists_Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _18008_Array_Rotation_Arrays_Lists_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
