b8c99228e51a2419a8034e3ab161b11b117733d6
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-29 00:08:02 +0300

    class Duplicated Added

---
+++ UnitTestingMethodsArraysLists_Exersize_App/Duplicates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingMethodsArraysLists_Exersize_App
+{
+    public class Duplicates
+    {
+        public static int[] RemoveDuplicates(int[] numbers)
+        {
+            HashSet<int> uniqueNumbers = new();
+
+            foreach (int number in numbers)
+            {
+                uniqueNumbers.Add(number);
+            }
+
+            return uniqueNumbers.ToArray();
+        }
+    }
+}
---
+++ UnitTestingMethodsArraysLists_Exersize_App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTestingMethodsArraysLists_Exersize_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
