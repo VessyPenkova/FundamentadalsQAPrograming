36f5cc442e23d4f0a223ebe7c8f248abd5f95925
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-30 09:04:54 +0200

    ExamPrep_1 Added

---
+++ CenterPoint/CenterPoint.cs
@@ -0,0 +1,40 @@
+﻿namespace CenterPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class CenterPoint
+        {
+            public static string GetClosest(double x1, double y1, double x2, double y2)
+            {
+                double pointOne = Math.Abs(x1) + Math.Abs(y1);
+                double pointTwo = Math.Abs(x2) + Math.Abs(y2);
+
+                string firstPointReport = $"({string.Join(", ", x1, y1)})";
+                string secondPointReport = $"({string.Join(", ", x2, y2)})";
+
+                if (pointOne > pointTwo)
+                {
+                    return secondPointReport;
+                }
+                else if (pointOne < pointTwo)
+                {
+                    return firstPointReport;
+                }
+                else
+                {
+                    if (x2 < 0 || y2 < 0)
+                    {
+                        return secondPointReport;
+                    }
+
+                    return firstPointReport;
+                }
+            }
+        }
+
+    }
