1bebb558903072dd0db0bfe22433b67ac8db175a
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-10-30 15:11:53 +0200

    2_MiddleElements Added

---
+++ 2._MiddleElements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2._MiddleElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            double sum = 0;
+            int position = input.Length / 2;
+
+            for (int i = 0; i <= input.Length / 2; i++)
+            {
+                if (i == position)
+                {
+                    int firstNum = input[i];
+                    int secondNum = input[i - 1];
+                    sum = firstNum + secondNum;
+                }
+               
+            }
+            Console.WriteLine($"{sum/2:f2}");
+        }
+    }
+}
